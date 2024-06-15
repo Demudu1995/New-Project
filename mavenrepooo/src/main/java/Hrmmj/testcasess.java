73e3873411b2b29ef40d8b1c1ff41267dddc4057
demud <demud@Donka>
2024-06-15 16:23:36 +0530

    second commit

--- mavenrepooo/src/main/java/Hrmmj/testcasess.java
+++ mavenrepooo/src/main/java/Hrmmj/testcasess.java
@@ -5,6 +5,7 @@ public class testcasess {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("testcase one");
+System.out.println("testcases 1234");
 	}
 
 }
